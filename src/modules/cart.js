0a107d98e66ce8191a189c570c38c680dad2d959
mikhail2013ru <mikhail2013ru@gmail.com>
2023-06-17 19:08:19 +0300

    корзина с товарами

--- src/modules/cart.js
+++ src/modules/cart.js
@@ -2,10 +2,43 @@ const cart = () => {
     const buttonCart = document.getElementById('cart-button')
     const modalCart = document.querySelector('.modal-cart')
     const close = modalCart.querySelector('.close')
+    const body = modalCart.querySelector('.modal-body')
+
+    const incrementCount = (id) => {
+        console.log(id)
+    }
+
+    const decrementCount = (id) => {
+        console.log(id)
+    }
     
     const renderItems = (data) => {
-        data.forEach(cartItem => {
-            console.log(cartItem)
+        body.innerHTML = ''
+
+        data.forEach(({ name, price, id, count }) => {
+            const cartElem = document.createElement('div')
+            cartElem.classList.add('food-row')
+            cartElem.innerHTML = `
+                <span class="food-name">${name}</span>
+                <strong class="food-price">${price} ₽</strong>
+                <div class="food-counter">
+                    <button class="counter-button btn-dec">-</button>
+                    <span class="counter">${count}</span>
+                    <button class="counter-button btn-inc">+</button>
+                </div>
+            `
+
+            cartElem.querySelector('.btn-dec').addEventListener('click', () => {
+                decrementCount(id)
+            })
+
+            cartElem.querySelector('.btn-inc').addEventListener('click', () => {
+                incrementCount(id)
+            })
+
+            body.append(cartElem)
+            console.log(cartElem)
+
         })
     }
 
