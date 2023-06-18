e34d644166f488c6f5f6abb280ff344127ad352a
mikhail2013ru <mikhail2013ru@gmail.com>
2023-06-18 13:00:35 +0300

    4 день

--- src/modules/cart.js
+++ src/modules/cart.js
@@ -3,13 +3,52 @@ const cart = () => {
     const modalCart = document.querySelector('.modal-cart')
     const close = modalCart.querySelector('.close')
     const body = modalCart.querySelector('.modal-body')
+    const buttonSend = modalCart.querySelector('.button-primary')
+    const clearCart = modalCart.querySelector('.clear-cart')
+
+    const resetCart = () => {
+        body.innerHTML = ''
+        localStorage.removeItem('cart')
+        closeModal()
+    }
+
+    console.log(buttonSend)
 
     const incrementCount = (id) => {
-        console.log(id)
+        const cartArray = JSON.parse(localStorage.getItem('cart'))
+        
+        cartArray.map((item) => {
+            if (item.id === id) {
+                item.count++
+                console.log(id)
+            }
+
+            return item
+        })
+
+        localStorage.setItem('cart', JSON.stringify(cartArray))
+        renderItems(cartArray)
     }
 
     const decrementCount = (id) => {
-        console.log(id)
+        const cartArray = JSON.parse(localStorage.getItem('cart'))
+        
+        cartArray.map((item) => {
+            if (item.id === id) {
+                item.count = item.count > 0 ? item.count - 1 : 0
+                // if (item.count > 0) {
+                //     item.count--
+                // } else {
+                //     item.count = 0
+                // }
+                // console.log(id)
+            }
+
+            return item
+        })
+
+        localStorage.setItem('cart', JSON.stringify(cartArray))
+        renderItems(cartArray)
     }
     
     const renderItems = (data) => {
@@ -22,19 +61,19 @@ const cart = () => {
                 <span class="food-name">${name}</span>
                 <strong class="food-price">${price} ₽</strong>
                 <div class="food-counter">
-                    <button class="counter-button btn-dec">-</button>
+                    <button class="counter-button btn-dec" data-index="${id}">-</button>
                     <span class="counter">${count}</span>
-                    <button class="counter-button btn-inc">+</button>
+                    <button class="counter-button btn-inc" data-index="${id}">+</button>
                 </div>
             `
 
-            cartElem.querySelector('.btn-dec').addEventListener('click', () => {
-                decrementCount(id)
-            })
+            // cartElem.querySelector('.btn-dec').addEventListener('click', () => {
+            //     decrementCount(id)
+            // })
 
-            cartElem.querySelector('.btn-inc').addEventListener('click', () => {
-                incrementCount(id)
-            })
+            // cartElem.querySelector('.btn-inc').addEventListener('click', () => {
+            //     incrementCount(id)
+            // })
 
             body.append(cartElem)
             console.log(cartElem)
@@ -42,12 +81,70 @@ const cart = () => {
         })
     }
 
+    body.addEventListener('click', (e) => {
+        e.preventDefault()
+        if (e.target.classList.contains('btn-inc')) {
+            incrementCount(e.target.dataset.index)
+        } else if (e.target.classList.contains('btn-dec')) {
+            decrementCount(e.target.dataset.index)
+        }
+    })
+
+    buttonSend.addEventListener('click', () => {
+        const cartArray = localStorage.getItem('cart')
+
+        if (JSON.stringify(cartArray) !== 'null') {
+            fetch('https://jsonplaceholder.typicode.com/posts', {
+                method: 'POST',
+                body: cartArray
+            })
+            .then(response => {
+                if (response.ok) {
+                    resetCart()
+                }
+            })
+            .catch(e => {
+                console.error(e)
+            })
+        } else {
+
+        }
+        // if (cartArray) {
+        //     console.log('nixera')
+        // } else {
+        //     fetch('https://jsonplaceholder.typicode.com/posts', {
+        //         method: 'POST',
+        //         body: cartArray
+        //     })
+        //     .then(response => {
+        //         if (response.ok) {
+        //             resetCart()
+        //         }
+        //     })
+        //     .catch(e => {
+        //         console.error(e)
+        //     })
+        // }
+
+    })
+
+    clearCart.addEventListener('click', () => {
+        body.innerHTML = ''
+        closeModal()
+    })
+
     buttonCart.addEventListener('click', () => {
        if (JSON.parse(localStorage.getItem('cart'))) {
             renderItems(JSON.parse(localStorage.getItem('cart')))
         }
 
         openModal()
+
+        const cartArray = JSON.parse(localStorage.getItem('cart'))
+        if (JSON.stringify(cartArray) === 'null') {
+            console.log('na xyi')
+            buttonSend.style.disable = true
+        }
     })
 
     const openModal = () => {
