c243ca4a18d1f57281138721bde56b1ce050a764
mikhail2013ru <mikhail2013ru@gmail.com>
2023-07-12 18:34:21 +0300

    фильтр товаров

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import auth from "./modules/auth";
 import partners from "./modules/partners";
 import slider from "./modules/slider";
 import cart from "./modules/cart";
-import inputSearch from "./modules/inputSearch"
+// import inputSearch from "./modules/inputSearch"
 
 auth()
 partners()
